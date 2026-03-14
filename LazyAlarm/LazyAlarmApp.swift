cf526fec8a6f1de496085bab0903c17992e36ff7
TadashiOtsu08 <hf.at.work2020@gmail.com>
2026-03-14 11:14:13 +0900

    Initial Commit

---
+++ LazyAlarm/LazyAlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyAlarmApp.swift
+//  LazyAlarm
+//
+//  Created by Confiance on 2026/03/14.
+//
+
+import SwiftUI
+
+@main
+struct LazyAlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LazyAlarmTests/LazyAlarmTests.swift
@@ -0,0 +1,17 @@
+//
+//  LazyAlarmTests.swift
+//  LazyAlarmTests
+//
+//  Created by Confiance on 2026/03/14.
+//
+
+import Testing
+@testable import LazyAlarm
+
+struct LazyAlarmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
